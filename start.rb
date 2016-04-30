19bef659e3571734ddfce667dc7ebccfa35633ce
Gabriel Santos <gabrielpelo@gmail.com>
2016-04-30 01:36:52 -0300

    Refactoring, code style

--- start.rb
+++ start.rb
@@ -36,54 +36,90 @@ def get_help
        }
 end
 
-exit_cmds = %w{ sair quit exit leave bye s q e l b }
-help_cmds = %w{ ajuda help info options a h i o ? }
+def calculate_multiple_values(t)
+  times = t.split.map { |s| Instant.new(s) }
 
-puts %{Bem vindo!
-Este programa calcula seu horário de saída.}
+  (2..times.length).each do |i|
+    a = times[i-2]
+    b = times[i-1]
+    c = b - a
+    print_multiple_results(a, b, c)
+    puts ""
+  end
+end
+
+def calculate_entrance_time(t)
+  a = Instant.new(t)
+  b = Instant.new("9:48")
+  c = a - b
+  d = a - Instant.now
+  print_result(c, a, d)
+end
+
+def calculate_exit_time(t)
+  a = Instant.new(t)
+  b = Instant.new("9:48")
+  c = a + b
+  d = c - Instant.now
+  print_result(a, c, d)
+end
+
+def print_result(a, b, c)
+  puts "    Entrada: #{a}"
+  puts "    Saida:   #{b}"
+  puts "    Faltam:  #{c}"
+end
+
+def print_multiple_results(a, b, c)
+  puts "    Entrada: #{a}"
+  puts "    Saida:   #{b}"
+  puts "    Total:   #{c}"
+end
+
+def exit_cmds
+  %w{ sair quit exit leave bye s q e l b }
+end
+
+def help_cmds
+  %w{ ajuda help info options a h i o ? }
+end
+
+def has_multiple_values?(t)
+  t.split.length > 1
+end
+
+def is_calling_help?(t)
+  help_cmds.include? t.downcase
+end
+
+def is_done?(t)
+  exit_cmds.include? t.downcase
+end
+
+def has_a_number?(t)
+  t.gsub(/\D/,'').length > 0
+end
+
+def should_calculate_entrance?(t)
+  t[0] == '-'
+end
+
+puts "Bem vindo!\nEste programa calcula seu horário de saída."
 
 begin
   print "\n  Que horas você entrou? \n  > "
   t = gets.chomp
   puts "\n"
 
-  if help_cmds.include? t.downcase
+  if is_calling_help? t
     get_help
-  elsif t.split.length > 1
-    times = []
-
-    t.split.each do |s|
-      times.push(Instant.new(s))
-    end
-
-    (0..times.length-2).each do |i|
-      a = times[i]
-      b = times[i+1]
-      c = b - a
-      puts "    Entrada: #{a}"
-      puts "    Saida:   #{b}"
-      puts "    Total:   #{c}"
-      puts ""
-    end
-  elsif t.gsub(/\D/,'') != ''
-    if t[0] == '-'
-      t = t[1..-1]
-      a = Instant.new(t.to_s)
-      b = Instant.new("9:48")
-      c = a - b
-      d = a - Instant.now
-      puts "    Entrada: #{c}"
-      puts "    Saida:   #{a}"
-      puts "    Faltam:  #{d}"
+  elsif has_multiple_values? t
+    calculate_multiple_values t
+  elsif has_a_number? t
+    if should_calculate_entrance? t
+      calculate_entrance_time t[1..-1]
     else
-      a = Instant.new(t)
-      b = Instant.new("9:48")
-      c = a + b
-      d = c - Instant.now
-      puts "    Entrada: #{a}"
-      puts "    Saida:   #{c}"
-      puts "    Faltam:  #{d}"
+      calculate_exit_time t
     end
   end
-
-end until exit_cmds.include? t.downcase
+end until is_done? t
